1ead95e758cc7d86b487c4af729278340c6727e2
alu0101160337 <alu0101160337@ull.edu.es>
2020-03-10 17:56:28 +0000

    Añadido pruebas unitarias e integración continua, así como codigo para parsear el contenido de un rdf

---
+++ databases/lib/parse-rdf.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const selector = require('cheerio');
+
+module.exports = rdf => {
+    const $ = selector.load(rdf);
+    const book = {};
+    book.id =  parseInt($('pgterms\\:ebook').attr('rdf:about').replace('ebooks/',''),10);
+    book.title = $("dcterms\\:title").text();
+    book.authors = $("pgterms\\:agent pgterms\\:name")
+        .toArray().map(element => $(element).text());
+    book.subjects = $('[rdf\\:resource$="/LCSH"]')
+        .parent().find('rdf\\:value')
+        .toArray().map(elem => $(elem).text());
+    return book;
+};
